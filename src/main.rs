db580563eaaaa86469db43c9d7c02114f1243020
Hugo Sibony <hugo.sibony@epita.fr>
2024-10-01 12:45:11 +0200

    Improved some formatting and fixed timezone handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use icalendar::Calendar;
 use icalendar::CalendarComponent::{Event, Todo, Venue};
+use now::DateTimeNow;
 use rrule::{RRuleSet, Tz};
 use std::{env, fs};
 
-use now::TimeZoneNow;
-
-use chrono::{Duration, FixedOffset, Local, NaiveDateTime, TimeZone};
+use chrono::{Duration, Local, NaiveDateTime, TimeZone};
 
 use itertools::Itertools;
 
@@ -17,7 +16,7 @@ struct AgendaEntry {
     duration: Duration,
 }
 
-fn fmt_duration(d: chrono::Duration) -> String {
+fn fmt_duration(d: Duration) -> String {
     if d.num_hours() != 0 {
         return format!("{}h", d.num_hours());
     }
@@ -52,24 +51,28 @@ fn fmt_agenda_entry(entry: AgendaEntry, when: NaiveDateTime) -> String {
 
 fn as_naive(dt: icalendar::CalendarDateTime) -> NaiveDateTime {
     match dt {
-        icalendar::CalendarDateTime::Floating(f) => return f,
-        icalendar::CalendarDateTime::Utc(u) => return u.naive_local(),
-        icalendar::CalendarDateTime::WithTimezone { date_time, tzid } => {
-            return date_time; // TODO: handle better
+        icalendar::CalendarDateTime::Floating(f) => f,
+        icalendar::CalendarDateTime::Utc(u) => {
+            Local.from_utc_datetime(&u.naive_utc()).naive_local()
         }
+        icalendar::CalendarDateTime::WithTimezone { date_time, tzid } => date_time,
     }
 }
 
 fn extract_event(
     event: &impl icalendar::Component,
-    sod: chrono::DateTime<Tz>,
-    eod: chrono::DateTime<Tz>,
-    offset: FixedOffset,
+    sod: chrono::DateTime<Local>,
+    eod: chrono::DateTime<Local>,
 ) -> Vec<AgendaEntry> {
     let naive_start: NaiveDateTime = match event.get_start() {
         Some(start_time) => match start_time {
             icalendar::DatePerhapsTime::DateTime(dt) => as_naive(dt),
-            icalendar::DatePerhapsTime::Date(d) => d.and_hms(0, 0, 0),
+            icalendar::DatePerhapsTime::Date(d) => Local
+                .from_local_date(&d)
+                .unwrap()
+                .and_hms_opt(0, 0, 0)
+                .unwrap()
+                .naive_local(),
         },
         None => return vec![], // TODO: error handle as event is missing a start time
     };
@@ -77,7 +80,9 @@ fn extract_event(
     let duration = match event.get_end() {
         Some(end_time) => match end_time {
             icalendar::DatePerhapsTime::DateTime(et) => as_naive(et) - naive_start,
-            icalendar::DatePerhapsTime::Date(_) => offset.end_of_day().naive_local() - naive_start,
+            icalendar::DatePerhapsTime::Date(_) => {
+                Local::now().end_of_day().naive_local() - naive_start
+            }
         },
         None => return vec![], // TODO: handle the case where we have a start time but
                                // are missing an end time better
@@ -102,14 +107,14 @@ fn extract_event(
     match props.parse::<RRuleSet>() {
         Ok(rrule) => {
             return rrule
-                .after(sod)
-                .before(eod)
+                .after(sod.with_timezone(&Tz::UTC))
+                .before(eod.with_timezone(&Tz::UTC))
                 .all(100)
                 .dates
                 .drain(..)
                 .map(|a| AgendaEntry {
                     name: event.get_summary().unwrap_or("").to_owned(),
-                    start: a.naive_local(),
+                    start: Local.from_utc_datetime(&a.naive_utc()).naive_local(),
                     duration,
                 })
                 .collect();
@@ -120,32 +125,29 @@ fn extract_event(
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Some(file_name) = env::args().nth(1) {
-        // TODO: Error handle better!
-        let file_contents = fs::read_to_string(file_name);
-        let parsed_calendar = file_contents?.parse::<Calendar>()?;
+        let file_contents = fs::read_to_string(file_name)?;
+        let parsed_calendar = file_contents.parse::<Calendar>()?;
 
-        let offset = Local::now().offset().clone();
-        let sod = Tz::UTC
-            .from_local_datetime(&offset.beginning_of_day().naive_local())
-            .unwrap();
-        let eod = sod + Duration::hours(32); // Tz::UTC.from_local_datetime(&offset.end_of_day().naive_local()).unwrap();
+        let now = Local::now();
+        let sod = now.date_naive().and_hms_opt(0, 0, 0).unwrap();
+        let eod = sod + Duration::hours(32);
 
-        let current_time = chrono::offset::Local.now().naive_local();
+        let current_time = now.naive_local();
 
         let ans: String = parsed_calendar
             .iter()
             .flat_map(|element| {
                 match element {
-                    Event(e) => extract_event(e, sod, eod, offset),
-                    Todo(t) => extract_event(t, sod, eod, offset),
-                    Venue(v) => extract_event(v, sod, eod, offset),
+                    Event(e) => extract_event(e, now, now + Duration::hours(32)),
+                    Todo(t) => extract_event(t, now, now + Duration::hours(32)),
+                    Venue(v) => extract_event(v, now, now + Duration::hours(32)),
                     &_ => vec![], // TODO: LOG!
                 }
             })
             .sorted_unstable_by_key(|item| item.start)
             .filter(|item| {
-                return (item.start + item.duration) >= current_time
-                    && (current_time - item.start).num_hours() < 24;
+                (item.start + item.duration) >= current_time
+                    && (current_time - item.start).num_hours() < 24
             })
             .take(2)
             .map(|item| fmt_agenda_entry(item, current_time))
@@ -154,5 +156,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         println!("{}", ans);
     }
-    return Ok(());
+    Ok(())
 }
